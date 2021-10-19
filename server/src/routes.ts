14a85a1737af70277afa77c4cbabfe181efde230
Anderson <andersonfonseca1999@hotmail.com>
2021-10-19 00:19:13 -0300

    Add Profile route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,7 +2,8 @@ import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateMessageController } from './controllers/CreateMessageController';
 import { GetLast3MessagesController } from './controllers/GetLast3MessagesController';
-import { ensureAutheticated } from './middleware/ensureAutheticated';
+import { ProfileUserController } from './controllers/ProfileUserController';
+import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 const router = Router();
 
@@ -10,10 +11,12 @@ router.post('/authenticate', new AuthenticateUserController().handle);
 
 router.post(
   '/messages',
-  ensureAutheticated,
+  ensureAuthenticated,
   new CreateMessageController().handle
 );
 
 router.get('/messages/last3', new GetLast3MessagesController().handle);
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle);
 
 export { router };

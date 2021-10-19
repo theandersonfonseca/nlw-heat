1f4a873ba77de32f288a9270ef33515b7600039e
Anderson <andersonfonseca1999@hotmail.com>
2021-10-18 23:13:08 -0300

    Add messages route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateMessageController } from './controllers/CreateMessageController';
+import { ensureAutheticated } from './middleware/ensureAutheticated';
 
 const router = Router();
 
 router.post('/authenticate', new AuthenticateUserController().handle);
+
+router.post(
+  '/messages',
+  ensureAutheticated,
+  new CreateMessageController().handle
+);
 
 export { router };

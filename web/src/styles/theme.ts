9d3b30d737cfb1c2b1e9026bfa3c1e4754f7afa0
Anderson <andersonfonseca1999@hotmail.com>
2021-10-19 22:25:13 -0300

    Add style theme

---
+++ web/src/styles/theme.ts
@@ -0,0 +1,47 @@
+export default {
+  font: {
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.4rem',
+      huge: '3.2rem',
+    },
+  },
+  colors: {
+    green: '#1B873F',
+    pink: '#FF008E',
+    orange: '#FF7A29',
+    yellow: '#FFCD1E',
+    black1: '#09090A',
+    black2: '#121214',
+    black3: '#202024',
+    gray1: '#8D8D99',
+    gray2: '#C4C4CC',
+    gray3: '#E1E1E6',
+    gray4: '#29292E',
+    white: '#FFFFFF',
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem',
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50,
+  },
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out',
+  },
+} as const;

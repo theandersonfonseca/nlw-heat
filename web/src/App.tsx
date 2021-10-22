eed6b9ff0c31519730fb0bf1b16139bd7e99829e
Anderson <andersonfonseca1999@hotmail.com>
2021-10-21 22:25:50 -0300

    Add components to App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,9 +2,22 @@ import GlobalStyles from './styles/global';
 import { ThemeProvider } from 'styled-components/macro';
 import theme from './styles/theme';
 
+import { Container } from './components/Container';
+import { MessageList } from './components/MessageList';
+import { LoginBox } from './components/LoginBox';
+import { SendMessageForm } from './components/SendMessageForm';
+
+import { useAuth } from './hooks/useAuth';
+
 function App() {
+  const { user } = useAuth();
+
   return (
     <ThemeProvider theme={theme}>
+      <Container signed={!!user}>
+        <MessageList />
+        {!!user ? <SendMessageForm /> : <LoginBox />}
+      </Container>
       <GlobalStyles />
     </ThemeProvider>
   );
